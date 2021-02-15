6cf2a5857a081ef3c781f6c2401f56f83c744eeb
glezzz <alexglezk@gmail.com>
2021-02-15 15:41:22 +0100

    initial commit

---
+++ src/main/java/com/shoppingcart/sbhibernateshoppingcart/SbhibernateshoppingcartApplication.java
@@ -0,0 +1,13 @@
+package com.shoppingcart.sbhibernateshoppingcart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbhibernateshoppingcartApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SbhibernateshoppingcartApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/shoppingcart/sbhibernateshoppingcart/SbhibernateshoppingcartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shoppingcart.sbhibernateshoppingcart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbhibernateshoppingcartApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
