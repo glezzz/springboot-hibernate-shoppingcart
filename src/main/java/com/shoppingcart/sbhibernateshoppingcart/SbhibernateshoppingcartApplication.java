0d1756d10ee5de9e7cbb7dbd0f9fdab7e35117e3
glezzz <alexglezk@gmail.com>
2021-02-16 15:32:20 +0100

    setting up database

--- src/main/java/com/shoppingcart/sbhibernateshoppingcart/SbhibernateshoppingcartApplication.java
+++ src/main/java/com/shoppingcart/sbhibernateshoppingcart/SbhibernateshoppingcartApplication.java
@@ -1,9 +1,19 @@
 package com.shoppingcart.sbhibernateshoppingcart;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 
 @SpringBootApplication
+
+@EnableAutoConfiguration(exclude = { //
+        DataSourceAutoConfiguration.class, //
+        DataSourceTransactionManagerAutoConfiguration.class, //
+        HibernateJpaAutoConfiguration.class })
+
 public class SbhibernateshoppingcartApplication {
 
     public static void main(String[] args) {
