3b320b9eec67b1a68da55d8a4b67bbf8177bcd32
glezzz <alexglezk@gmail.com>
2021-02-16 16:32:39 +0100

    implementing roles & access

---
+++ src/main/java/com/shoppingcart/sbhibernateshoppingcart/config/WebSecurityConfig.java
@@ -0,0 +1,45 @@
+package com.shoppingcart.sbhibernateshoppingcart.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@Configuration
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    UserDetailsServiceImpl userDetailsService;
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder();
+        return bCryptPasswordEncoder;
+    }
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        // Setting Service to find User in the database.
+        // And Setting PasswordEncoder
+        auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+
+        http.csrf().disable();
+        // Requires login with role ROLE_EMPLOYEE or ROLE_MANAGER.
+        // If not, it will redirect to /admin/login.
+        http.authorizeRequests().antMatchers("/admin/orderList", "/admin/order", "admin/accountInfo").access("hasAnyRole('ROLE_EMPLOYEE', 'ROLE_MANAGER')");
+
+        //Pages only for MANAGER
+        http.authorizeRequests().antMatchers("admin/product").access("hasRole('ROLE_MANAGER')");
+
+        // When user login, role XX.
+        // But access to the page requires the YY role,
+        // An AccessDeniedException will be thrown.
+    }
+}
