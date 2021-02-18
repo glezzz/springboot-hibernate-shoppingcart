47de48503e1099553ec1a3d991347634594a9941
glezzz <alexglezk@gmail.com>
2021-02-18 14:43:48 +0100

    working on main controller

---
+++ src/main/java/com/shoppingcart/sbhibernateshoppingcart/controller/AdminController.java
@@ -0,0 +1,4 @@
+package com.shoppingcart.sbhibernateshoppingcart.controller;
+
+public class AdminController {
+}
--- src/main/java/com/shoppingcart/sbhibernateshoppingcart/controller/MainController.java
+++ src/main/java/com/shoppingcart/sbhibernateshoppingcart/controller/MainController.java
@@ -1,15 +1,79 @@
 package com.shoppingcart.sbhibernateshoppingcart.controller;
 
+import com.shoppingcart.sbhibernateshoppingcart.dao.OrderDAO;
+import com.shoppingcart.sbhibernateshoppingcart.dao.ProductDAO;
+import com.shoppingcart.sbhibernateshoppingcart.form.CustomerForm;
+import com.shoppingcart.sbhibernateshoppingcart.model.CartInfo;
+import com.shoppingcart.sbhibernateshoppingcart.model.ProductInfo;
+import com.shoppingcart.sbhibernateshoppingcart.pagination.PaginationResult;
+import com.shoppingcart.sbhibernateshoppingcart.validator.CustomerFormValidator;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
+@Transactional
 public class MainController {
 
-    @GetMapping("/")
+    @Autowired
+    private OrderDAO orderDAO;
+
+    @Autowired
+    private ProductDAO productDAO;
+
+    @Autowired
+    private CustomerFormValidator customerFormValidator;
+
+    @InitBinder
+    public void myInitBinder(WebDataBinder dataBinder) {
+        Object target = dataBinder.getTarget();
+        if (target == null) {
+            return;
+        }
+        System.out.println("Target = " + target);
+
+        // Case update quantity in cart
+        // (@ModelAttribute("cartForm") @Validated CartInfo cartForm)
+        if (target.getClass() == CartInfo.class) {
+
+        }
+        // Case save customer information.
+        // (@ModelAttribute @Validated CustomerInfo customerForm)
+        else if (target.getClass() == CustomerForm.class) {
+            dataBinder.setValidator(customerFormValidator);
+        }
+    }
+
+    @RequestMapping("/403")
+    public String accessDenied() {
+        return "/403";
+    }
+
+    @RequestMapping
     public String home() {
         return "index";
     }
 
+    //Product List
+    @RequestMapping({"/productList"})
+    public String listProductHandler(Model model,
+                                     @RequestParam(value = "name", defaultValue = "") String likeName,
+                                     @RequestParam(value = "page", defaultValue = "1") int page) {
+        final int maxResult = 5;
+        final int maxNavigationPage = 10;
+
+        PaginationResult<ProductInfo> result = productDAO.queryProducts(page, //
+                maxResult, maxNavigationPage, likeName);
+
+        model.addAttribute("paginationProducts", result);
+        return "productList";
+
+
+    }
 }
