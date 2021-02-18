7da90753cf472f5b67dbe1edcf59c950142005f4
glezzz <alexglezk@gmail.com>
2021-02-18 16:31:10 +0100

    working on controller & templates

--- src/main/java/com/shoppingcart/sbhibernateshoppingcart/controller/MainController.java
+++ src/main/java/com/shoppingcart/sbhibernateshoppingcart/controller/MainController.java
@@ -2,20 +2,25 @@ package com.shoppingcart.sbhibernateshoppingcart.controller;
 
 import com.shoppingcart.sbhibernateshoppingcart.dao.OrderDAO;
 import com.shoppingcart.sbhibernateshoppingcart.dao.ProductDAO;
+import com.shoppingcart.sbhibernateshoppingcart.entity.Product;
 import com.shoppingcart.sbhibernateshoppingcart.form.CustomerForm;
 import com.shoppingcart.sbhibernateshoppingcart.model.CartInfo;
+import com.shoppingcart.sbhibernateshoppingcart.model.CustomerInfo;
 import com.shoppingcart.sbhibernateshoppingcart.model.ProductInfo;
 import com.shoppingcart.sbhibernateshoppingcart.pagination.PaginationResult;
+import com.shoppingcart.sbhibernateshoppingcart.utils.Utils;
 import com.shoppingcart.sbhibernateshoppingcart.validator.CustomerFormValidator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.WebDataBinder;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.InitBinder;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 @Transactional
@@ -73,7 +78,99 @@ public class MainController {
 
         model.addAttribute("paginationProducts", result);
         return "productList";
+    }
 
+    @RequestMapping({"/buyProduct"})
+    public String listProductHandler(HttpServletRequest request, Model model,
+                                     @RequestParam(value = "code", defaultValue = "") String code) {
 
+        Product product = null;
+        if (code != null && code.length() > 0) {
+            product = productDAO.findProduct(code);
+        }
+
+        if (product != null) {
+            CartInfo cartInfo = Utils.getCartInSession(request);
+
+            ProductInfo productInfo = new ProductInfo(product);
+
+            cartInfo.addProduct(productInfo, 1);
+        }
+        return "redirect:/shoppingCart";
+    }
+
+    @RequestMapping({"/shoppingCartRemoveProduct"})
+    public String removeProductHandler(HttpServletRequest request, Model model,
+                                       @RequestParam(value = "code", defaultValue = "") String code) {
+        Product product = null;
+        if (code != null && code.length() > 0) {
+            product = productDAO.findProduct(code);
+        }
+
+        if (product != null) {
+            CartInfo cartInfo = Utils.getCartInSession(request);
+
+            ProductInfo productInfo = new ProductInfo(product);
+
+            cartInfo.removeProduct(productInfo);
+        }
+        return "redirect:/shoppingCart";
+    }
+
+    //POST: Update quantity for product in cart
+    @RequestMapping(value = { "/shoppingCart" }, method = RequestMethod.POST)
+    public String shoppingCartUpdateQty(HttpServletRequest request, Model model,
+                                        @ModelAttribute("cartForm") CartInfo cartForm) {
+
+        CartInfo cartInfo = Utils.getCartInSession(request);
+        cartInfo.updateQuantity(cartForm);
+
+        return "redirect:/shoppingCart";
+    }
+
+    //GET: Show cart
+    @RequestMapping(value = {"/shoppingCart"}, method = RequestMethod.GET)
+    public String shoppingCartHandler(HttpServletRequest request, Model model) {
+        CartInfo myCart = Utils.getCartInSession(request);
+
+        model.addAttribute("cartForm", myCart);
+        return "shoppingCart";
+    }
+
+    //GET: Enter customer information
+    @RequestMapping(value = {"/shoppingCartCustomer"}, method = RequestMethod.GET)
+    public String shoppingCartCustomerForm(HttpServletRequest request, Model model) {
+
+        CartInfo cartInfo = Utils.getCartInSession(request);
+
+        if (cartInfo.isEmpty()) {
+            return "redirect:/shoppingCart";
+        }
+        CustomerInfo customerInfo = cartInfo.getCustomerInfo();
+
+        CustomerForm customerForm = new CustomerForm(customerInfo);
+
+        model.addAttribute("customerForm", customerForm);
+
+        return "shoppingCartCustomer";
+    }
+
+    //POST: Save customer information
+    @RequestMapping(value = {"/shoppingCartCustomer"}, method = RequestMethod.POST)
+    public String shoppingCustomerSave(HttpServletRequest request, Model model,
+                                       @ModelAttribute("customerForm") @Validated CustomerForm customerForm,
+                                       BindingResult result, final RedirectAttributes redirectAttributes) {
+        if (result.hasErrors()) {
+            customerForm.setValid(false);
+            //Forward to reenter customer info
+            return "shoppingCartCustomer";
+        }
+
+        customerForm.setValid(true);
+        CartInfo cartInfo = Utils.getCartInSession(request);
+        CustomerInfo customerInfo = new CustomerInfo(customerForm);
+        cartInfo.setCustomerInfo(customerInfo);
+
+        return "redirect:/shoppingCartConfirmation";
     }
 }
